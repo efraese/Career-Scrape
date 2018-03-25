64aa02a9ab3b3e989ae1c9c3aa38f6e3c49091e0
Eric Fraese <fuzzyeric30@Erics-MacBook-Pro.local>
2018-03-24 23:32:36 -0400

    all data collected for my program

--- careerslogin.py
+++ careerslogin.py
@@ -25,7 +25,6 @@ driver.implicitly_wait(3)
 for_my_program = driver.find_element_by_xpath("""//*[@id="quickSearchCountsContainer"]/table[1]/tbody/tr[1]/td[2]/a""")
 for_my_program.click()
 driver.implicitly_wait(3)
-jobs = driver.find_elements_by_class_name("searchResult")
 
 filename = "jobdata.csv"
 f = open(filename, "w")
@@ -34,22 +33,34 @@ headers = "job_title, company_name, state, city, link \n"
 f.write(headers)
 
 base_url = "https://nucareers.northeastern.edu/postings.htm?pId="
-for job in jobs:
-    tds = job.find_elements_by_tag_name("td")
-    job_title = tds[3].text.replace(",", " ").replace("ä", "a")
 
-    job_link = base_url + job.get_attribute("id")[7:]
+pages = driver.find_elements_by_class_name("orbis-posting-actions")[3].find_elements_by_tag_name("li")
+num_pages_remaining = len(pages)-4
+old_job = ""
+new_job = ""
+while(num_pages_remaining > 0):
+    while(old_job == new_job):
+        jobs = driver.find_elements_by_class_name("searchResult")
+        new_job = jobs[0]
+    for job in jobs:
+        tds = job.find_elements_by_tag_name("td")
+        job_title = tds[3].text.replace(",", " ").replace("ã", "a")
 
-    company_name = tds[4].text.replace(",", " ").replace("ä", "a")
+        job_link = base_url + job.get_attribute("id")[7:]
 
-    state = tds[8].text.replace(",", " ").replace("ä", "a")
-    city = tds[9].text.replace(",", " ").replace("ä", "a")
+        company_name = tds[4].text.replace(",", " ").replace("ä", "a").replace("ã", "a")
 
-    row = job_title + "," + company_name + "," + state + "," + city + "," + job_link + "\n"
+        state = tds[8].text.replace(",", " ").replace("ä", "a").replace("ã", "a")
+        city = tds[9].text.replace(",", " ").replace("ä", "a").replace("ã", "a")
 
-    #print(row)
-    f.write(row)
-    print(company_name)
+        row = job_title + "," + company_name + "," + state + "," + city + "," + job_link + "\n"
+
+        f.write(row)
+    next_page = driver.find_element_by_xpath("""//*[@id="postingsTablePlaceholder"]/div[4]/div/ul/li[""" +str(len(pages)-1)+ """]/a""")
+    next_page.click()
+    driver.implicitly_wait(3)
+    num_pages_remaining -= 1
+    old_job = new_job
     
 f.close()
 
